023ec28f8ebedf0ac20d158bcaf13abb503a0cf3
Hamilton Schat Junior <hamilton.schat@gmail.com>
2025-07-09 15:27:41 -0300

    corrige import do CORS

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, request, jsonify, render_template, CORS
+from flask import Flask, request, jsonify, render_template
+from flask_cors import CORS
 import json
 import os
 from datetime import datetime
